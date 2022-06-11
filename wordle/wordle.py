3b6a8075eebb87d83afbbc7d707d498a7ff0bcb7
Kevin Burrell <kevinburrell@Kevins-Air.home>
2022-06-11 13:17:14 -0400

    Use strings instead of lists for regex letters and fix required letters

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -1,42 +1,46 @@
 import re
 import string
-from collections import defaultdict
+import copy
 
 def regex_builder(possible_letters):
     """Builds"""
     regex_str = ''
-    for letter in possible_letters.values():
-        regex_str = regex_str + f'[{"".join(letter)}]'
+    for letters in possible_letters:
+        regex_str = regex_str + f'[{letters}]'
     return regex_str
 
-with open('wordle.txt', 'r') as fh:
+with open('wordle/wordle.txt', 'r') as fh:
     words = fh.read()
 
 correct_word = False
-valid_letter_position = defaultdict(list)
-for i in range(5):
-    valid_letter_position[i] = list(string.ascii_lowercase)
-
-while not correct_word:
+valid_letter_position = [string.ascii_lowercase] * 5
+must_have_letters = []
+match = []
+while not len(match) == 1:
     guess_coded = input("Guess:coded(gyb)").partition(':')
     guess = guess_coded[0].lower()
     coded = guess_coded[2].lower()
     for i, letter in enumerate(guess):   
         if coded[i] == 'b':
             for j in range(4):
-                if letter in valid_letter_position[j]:
-                    valid_letter_position[j].remove(letter)
+                valid_letter_position[j] = valid_letter_position[j].replace(letter, '')
         elif coded[i] == 'y':
-            if letter in valid_letter_position[i]:
-                valid_letter_position[i].remove(letter)
+            if letter not in must_have_letters:
+                must_have_letters.append(letter)
+            valid_letter_position[i] = valid_letter_position[i].replace(letter, '')
         elif coded[i] == 'g':
-            valid_letter_position[i] = [letter]
+            valid_letter_position[i] = letter
         else:
             pass
     regex = regex_builder(valid_letter_position)
     match = re.findall(regex, words)
+    for word in copy.deepcopy(match):
+        for letter in must_have_letters:
+            if letter not in word:
+                match.remove(word)
     print(len(match))
     print(match)
+    
 
 
 
